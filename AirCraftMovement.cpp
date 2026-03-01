03574e6ddbb9fd1891e35f6f13d2269efd1ab76f
Tejas Dalvi <83655902+TejasDalvi10100@users.noreply.github.com>
2026-03-01 14:44:38 +0530

    Source code

---
+++ AirCraftMovement.cpp
@@ -0,0 +1,42 @@
+int en = 11, in1 = 7, in2 = 8; 
+
+void setup() {
+// put your setup
+  code here, to run once:
+pinMode(en, OUTPUT);
+pinMode(in1, OUTPUT);
+pinMode(in2,
+  OUTPUT);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+digitalWrite(in1,
+  LOW);
+digitalWrite(in2, HIGH);
+
+analogWrite(en, 96);
+
+delay(14000);
+
+digitalWrite(in1,
+  LOW);
+digitalWrite(in2, LOW);
+
+delay(5000);
+
+digitalWrite(in1, HIGH);
+digitalWrite(in2,
+  LOW);
+
+analogWrite(en, 180);
+
+delay(10500);
+
+digitalWrite(in1,
+  LOW);
+digitalWrite(in2, LOW);
+
+delay(8000);
+
+}
---
+++ IndicatorController.cpp
@@ -0,0 +1,82 @@
+int irInner=6,irMiddle=13,irOuter=12,irRunway=9;
+int ledInner=2,ledMiddle=3,ledOuter=4;
+int
+  sounds = 11; // Declaration of variables with pin numbers of components
+int valIn=0,valMd=0,valOt=0,valRunway=0;
+void
+  setup()
+{
+ pinMode(irInner, INPUT_PULLUP); // input component
+ pinMode(ledInner,
+  OUTPUT); //output component
+ 
+ pinMode(irMiddle, INPUT_PULLUP);
+ pinMode(ledMiddle,
+  OUTPUT);
+ 
+ pinMode(irOuter, INPUT_PULLUP);
+ pinMode(ledOuter, OUTPUT);
+
+  pinMode(irRunway,INPUT_PULLUP); 
+}
+void loop()
+{
+ valIn=digitalRead(irInner);
+  //getting current state of sensor(HIGH or LOW)
+ valMd=digitalRead(irMiddle);
+
+  valOt=digitalRead(irOuter);
+ valRunway=digitalRead(irRunway);
+ if(valOt==LOW
+  ) //if obstacle on outer sensor
+ {
+35
+ digitalWrite(ledOuter, HIGH); //led
+  on
+ tone(sounds, 1500, 350); //buzzer or speaker starts beeping sound
+ delay(350);
+
+  }
+ else
+ {
+ digitalWrite(ledOuter,LOW); //led off
+ }
+ delay(50);
+
+  
+ if(valMd==LOW )
+ { 
+ digitalWrite(ledMiddle, HIGH);
+ tone(sounds,
+  1200, 100);
+ delay(200);
+ tone(sounds, 1100, 450);
+ delay(250);
+ }
+
+  else
+ {
+ digitalWrite(ledMiddle, LOW);
+ }
+ delay(50);
+ 
+ if(valIn==LOW
+  )
+ { 
+ digitalWrite(ledInner, HIGH);
+ tone(sounds, 1000, 100);
+ delay(100);
+
+  } 
+ else
+ {
+ digitalWrite(ledInner, LOW);
+ }
+ delay(50);
+ 
+
+  if(valRunway==LOW)
+ { 
+ delay(10000);
+ } 
+}
